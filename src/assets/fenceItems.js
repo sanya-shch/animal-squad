082e19f10aa40ca44a902cccd859d126ef854a6e
sanya-shch <shchipkov.sanya@ukr.net>
2023-03-05 20:31:13 +0200

    added fence images

---
+++ src/assets/fenceItems.js
@@ -0,0 +1,25 @@
+import fence1 from "./fence/fence_1.png";
+import fence2 from "./fence/fence_2.png";
+import fence3 from "./fence/fence_3.png";
+import fence4 from "./fence/fence_4.png";
+import fence5 from "./fence/fence_5.png";
+import fence6 from "./fence/fence_6.png";
+import fence7 from "./fence/fence_7.png";
+import fence8 from "./fence/fence_8.png";
+import fence9 from "./fence/fence_9.png";
+import fence10 from "./fence/fence_10.png";
+import fence11 from "./fence/fence_11.png";
+
+export {
+  fence1,
+  fence2,
+  fence3,
+  fence4,
+  fence5,
+  fence6,
+  fence7,
+  fence8,
+  fence9,
+  fence10,
+  fence11,
+};
