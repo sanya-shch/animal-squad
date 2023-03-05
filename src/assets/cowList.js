4f1bb5e30680dc430ba45a32a556bd30434cb7c3
sanya-shch <shchipkov.sanya@ukr.net>
2023-03-06 01:27:10 +0200

    added cows images

---
+++ src/assets/cowList.js
@@ -0,0 +1,45 @@
+import cow_1 from "./cow/cow_1.png";
+import cow_2 from "./cow/cow_2.png";
+import cow_3 from "./cow/cow_3.png";
+import cow_4 from "./cow/cow_4.png";
+import cow_5 from "./cow/cow_5.png";
+import cow_6 from "./cow/cow_6.png";
+import cow_7 from "./cow/cow_7.png";
+import cow_8 from "./cow/cow_8.png";
+import cow_9 from "./cow/cow_9.png";
+import cow_10 from "./cow/cow_10.png";
+import cow_11 from "./cow/cow_11.png";
+import cow_12 from "./cow/cow_12.png";
+import cow_13 from "./cow/cow_13.png";
+import cow_14 from "./cow/cow_14.png";
+import cow_15 from "./cow/cow_15.png";
+import cow_16 from "./cow/cow_16.png";
+import cow_17 from "./cow/cow_17.png";
+import cow_01 from "./cow/cow_01.png";
+import cow_02 from "./cow/cow_02.png";
+import cow_03 from "./cow/cow_03.png";
+import cow_04 from "./cow/cow_04.png";
+
+export {
+  cow_1,
+  cow_2,
+  cow_3,
+  cow_4,
+  cow_5,
+  cow_6,
+  cow_7,
+  cow_8,
+  cow_9,
+  cow_10,
+  cow_11,
+  cow_12,
+  cow_13,
+  cow_14,
+  cow_15,
+  cow_16,
+  cow_17,
+  cow_01,
+  cow_02,
+  cow_03,
+  cow_04,
+};
