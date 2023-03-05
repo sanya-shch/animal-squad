e00f0231cf037276340e2bfc33a67df5e8ff842f
sanya-shch <shchipkov.sanya@ukr.net>
2023-03-05 14:00:05 +0200

    added pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "../src/screens/Home";
+import GamePage from "../src/screens/Game";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/game" element={<GamePage />} />
+    </Routes>
+  );
 }
 
 export default App;
