f83c558f8bfa31f5fd140dbe77582c768a5477c3
sanya-shch <shchipkov.sanya@ukr.net>
2023-03-07 00:26:06 +0200

    added cows images

--- src/assets/cowList.js
+++ src/assets/cowList.js
@@ -15,6 +15,46 @@ import cow_14 from "./cow/cow_14.png";
 import cow_15 from "./cow/cow_15.png";
 import cow_16 from "./cow/cow_16.png";
 import cow_17 from "./cow/cow_17.png";
+import cow_18 from "./cow/cow_18.png";
+import cow_19 from "./cow/cow_19.png";
+import cow_20 from "./cow/cow_20.png";
+import cow_21 from "./cow/cow_21.png";
+import cow_22 from "./cow/cow_22.png";
+import cow_23 from "./cow/cow_23.png";
+import cow_24 from "./cow/cow_24.png";
+import cow_25 from "./cow/cow_25.png";
+import cow_26 from "./cow/cow_26.png";
+import cow_27 from "./cow/cow_27.png";
+import cow_28 from "./cow/cow_28.png";
+import cow_29 from "./cow/cow_29.png";
+import cow_30 from "./cow/cow_30.png";
+import cow_31 from "./cow/cow_31.png";
+import cow_32 from "./cow/cow_32.png";
+import cow_33 from "./cow/cow_33.png";
+import cow_34 from "./cow/cow_34.png";
+import cow_35 from "./cow/cow_35.png";
+import cow_36 from "./cow/cow_36.png";
+import cow_37 from "./cow/cow_37.png";
+import cow_38 from "./cow/cow_38.png";
+import cow_39 from "./cow/cow_39.png";
+import cow_40 from "./cow/cow_40.png";
+import cow_41 from "./cow/cow_41.png";
+import cow_42 from "./cow/cow_42.png";
+import cow_43 from "./cow/cow_43.png";
+import cow_44 from "./cow/cow_44.png";
+import cow_45 from "./cow/cow_45.png";
+import cow_46 from "./cow/cow_46.png";
+import cow_47 from "./cow/cow_47.png";
+import cow_48 from "./cow/cow_48.png";
+import cow_49 from "./cow/cow_49.png";
+import cow_50 from "./cow/cow_50.png";
+import cow_51 from "./cow/cow_51.png";
+import cow_52 from "./cow/cow_52.png";
+import cow_53 from "./cow/cow_53.png";
+import cow_54 from "./cow/cow_54.png";
+import cow_55 from "./cow/cow_55.png";
+import cow_56 from "./cow/cow_56.png";
+
 import cow_01 from "./cow/cow_01.png";
 import cow_02 from "./cow/cow_02.png";
 import cow_03 from "./cow/cow_03.png";
@@ -38,6 +78,45 @@ export {
   cow_15,
   cow_16,
   cow_17,
+  cow_18,
+  cow_19,
+  cow_20,
+  cow_21,
+  cow_22,
+  cow_23,
+  cow_24,
+  cow_25,
+  cow_26,
+  cow_27,
+  cow_28,
+  cow_29,
+  cow_30,
+  cow_31,
+  cow_32,
+  cow_33,
+  cow_34,
+  cow_35,
+  cow_36,
+  cow_37,
+  cow_38,
+  cow_39,
+  cow_40,
+  cow_41,
+  cow_42,
+  cow_43,
+  cow_44,
+  cow_45,
+  cow_46,
+  cow_47,
+  cow_48,
+  cow_49,
+  cow_50,
+  cow_51,
+  cow_52,
+  cow_53,
+  cow_54,
+  cow_55,
+  cow_56,
   cow_01,
   cow_02,
   cow_03,
